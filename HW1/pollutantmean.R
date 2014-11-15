f6f694a9d70c6bcccfa48c1be6fa171e39e8764c
Eric Somdahl <ericsomdahl@gmail.com>
2014-11-15 12:03:04 -0500

    passed submit script tests

--- HW1/pollutantmean.R
+++ HW1/pollutantmean.R
@@ -11,4 +11,21 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
+  
+  ##figure out which filenames to read
+  filenames <- sprintf("./specdata/%03d.csv", id)
+  
+  ##read them in
+  ldf <- lapply(filenames, function(f) read.csv(f, header=TRUE))
+  
+  ##create a vector of means for each file
+  meanf <- lapply(ldf, function(x) filteredmean(x, pollutant))
+
+  ##get the mean of the vector
+  mean(unlist(meanf))
+}
+
+filteredmean <- function(df, pollutant) {
+  non_na <- subset(df, !is.na(df[,pollutant]))
+  non_na[,pollutant]
 }
