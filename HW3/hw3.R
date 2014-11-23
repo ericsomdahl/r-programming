3ffaf0d6fe10a3de87e35f672bd36714420d034b
Eric Somdahl <ericsomdahl@gmail.com>
2014-11-23 13:59:54 -0500

    validate outcome, stop shadowing input variable

--- HW3/hw3.R
+++ HW3/hw3.R
@@ -1,9 +1,10 @@
 best <- function(state, outcome) {
   ## Read outcome data
-  outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
   ## Check that state and outcome are valid
-  validateState(state, unique(outcome[, 7]))  
+  validateState(state, unique(outcome_data[, 7]))
+  validateOutcome(outcome)
   
   ## Return hospital name in that state with lowest 30-day death
   ## rate
@@ -12,5 +13,12 @@ best <- function(state, outcome) {
 validateState <- function(in_state, allowable_states) {
   if (is.element(in_state, allowable_states) == FALSE) {
     stop("invalid state")
+  }
+}
+
+validateOutcome <- function(in_outcome) {
+  validOutcomes <- c("heart attack", "heart failure", "pneumonia")
+  if (is.element(in_outcome, validOutcomes) == FALSE) {
+    stop("invalid outcome")
   }
 }
