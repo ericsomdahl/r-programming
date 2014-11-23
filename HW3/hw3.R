302959e5dd3f0e6781e5a357eb338b4f8c24953d
Eric Somdahl <ericsomdahl@gmail.com>
2014-11-23 13:49:05 -0500

    check input state

--- HW3/hw3.R
+++ HW3/hw3.R
@@ -1,6 +1,16 @@
 best <- function(state, outcome) {
   ## Read outcome data
+  outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
   ## Check that state and outcome are valid
+  validateState(state, unique(outcome[, 7]))  
+  
   ## Return hospital name in that state with lowest 30-day death
   ## rate
+}
+
+validateState <- function(in_state, allowable_states) {
+  if (is.element(in_state, allowable_states) == FALSE) {
+    stop("invalid state")
+  }
 }
